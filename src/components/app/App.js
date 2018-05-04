536bf24a8f65c3399b759f957b6f665899735113
Erwan Riou <riou@viamundus.com>
2018-05-04 19:04:45 +0200

    adding book statement, importing API in it

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 import SearchBar from '../searchbar/SearchBar.js';
 import BooksList from '../bookslist/BooksList.js';
+import * as BookAPI from '../../util/bookAPI.js';
 
-class App extends React.Component {
+class App extends React.Component  {
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    BookAPI.getAll().then(books => this.setState({books: books}));
+  }
+
+
+
   render() {
     return (
       <div>
